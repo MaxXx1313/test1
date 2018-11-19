2def22b159c0e675a74bf8bbcc6a6e1b8391abc5
Liuva <g.l.v.9816@gmail.com>
2018-11-19 15:26:30 +0300

    add calculator function

--- calculate_deposit/script.js
+++ calculate_deposit/script.js
@@ -1,5 +1,5 @@
- var INPUT_DATA = new Array(document.getElementById("AmountOfMoney"), document.getElementById("NumberOfMonth"),
-                            document.getElementById("StartMonth"), document.getElementById("SimplePercent"), document.getElementById("ComplexPercent"));
+ var INPUT_DATA = new Array(document.getElementById("AmountOfMoney"), document.getElementById("NumberOfMonth"),document.getElementById("StartMonth"),
+                            document.getElementById("SimplePercent"), document.getElementById("ComplexPercent"), document.getElementById("CompoundingPeriod"));
 
 //---------------------------------------validation of input data---------------------------------------------------------------
 
@@ -20,6 +20,7 @@ function isNumeric(n) {
     }
 
 //check StartMounth data for not empty field
+
     INPUT_DATA[2].classList.remove("valide_input");
 
     if (INPUT_DATA[2].value != ""){
@@ -27,18 +28,59 @@ function isNumeric(n) {
         INPUT_DATA[2].classList.add("valide_input");
     }
 
-//check NumderOfMonth data on integer    
-    if ((INPUT_DATA[1].value%1 != 0)){
-        INPUT_DATA[1].classList.remove("valide_input");
-        INPUT_DATA[1].classList.add("invalide_input");
-    }
+//check NumderOfMonth data on integer   
+    var integersInput=[1,5];
+    integersInput.forEach(function(int){
+        if ((INPUT_DATA[int].value%1 != 0)){
+            INPUT_DATA[int].classList.remove("valide_input");
+            INPUT_DATA[int].classList.add("invalide_input");
+        }
+    });
  }
-
 
 
 //----------------------------------------clearing form fields ---------------------------------------------------------------
 
 
+document.getElementById("button_clear-fields").addEventListener("click", function(){
+    INPUT_DATA.forEach(function(input){
+        input.value = "";
+        input.className = "";
+    });
+});
 
 
 
+//---------------------------------------calculation percents functions----------------------------------------------------------------
+
+
+// associative massive [[Simple Procent Money,Complex Procent Money] : month]
+
+
+
+//calculation of simple percent 
+function simplePercent(month,percent,startSum){
+    var previouseMonth=moneySimpleComplex[Object.keys(moneySimpleComplex)[startMonth.stepUp(month-1)],0];
+   Return(previouseMonth+startSum*percent/1200);
+}
+
+//calculation of complex percent
+function complexPercent(month,percent,compoundingPeriod){
+    var previouseMonth=moneySimpleComplex[Object.keys(moneySimpleComplex)[startMonth.stepUp(month-1)],1];
+    if(month%numOfCompoundPeriods!=0){
+        Return(previouseMonth);
+    };
+    else{
+        Return(previouseMonth(1+percent*compoundingPeriod/1200));
+    };
+}
+
+//calculation of all percents
+
+function calculateAll(numderOfMonth,AmountOfMoney,StartMonth,SimplePercent,ComplexPercent,CompoundingPeriod){
+    var moneySimpleComplex = Array();
+    moneySimpleComplex[startMonth] = [startSum,startSum];
+    
+
+
+}
